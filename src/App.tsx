ce327cc9bec7d826682f0107beb8c4c09d0376ba
bicky33 <biki.hamzah@gmail.com>
2023-05-10 03:59:43 +0700

    simple clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+
+function useTime() {
+    const [time, setTime] = useState(new Date());
+    useEffect(() => {
+      const id = setInterval(() => {
+        setTime(new Date());
+      }, 1000);
+    }, []);
+    return time;
+}
+
+function Clock({ color, time }:any) {
+    return (
+      <h1 style={{ color: color }}>
+        {time}
+      </h1>
+    );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
+    const time = useTime();
+    const [color, setColor] = useState('lightcoral');
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Pick a color:{' '}
+          <select value={color} onChange={e => setColor(e.target.value)}>
+            <option value="lightcoral">lightcoral</option>
+            <option value="midnightblue">midnightblue</option>
+            <option value="rebeccapurple">rebeccapurple</option>
+          </select>
         </p>
+        <Clock color={color} time={time.toLocaleTimeString()} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    )
 }
 
 export default App
